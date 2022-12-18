8968a25516e3763fcc69c90f4e59cd3259a9adc7
Prohit <udkarprohit@gmail.com>
2022-12-18 09:59:12 +0530

    Program3: Adding two Random Dice Number and Printing the Result

--- Day5_Prob01/Add_Two_Dice_Num.sh
+++ Day5_Prob01/Add_Two_Dice_Num.sh
@@ -2,7 +2,7 @@
 
 # Add two Random Dice Number and Print the Result
 
-num_One=$((RANDOM%6+1));
+num_One=$((RANDOM%6+1)); # 1 + 6
 num_Two=$((RANDOM%6+1));
 rel=$(($num_One + $num_Two));
 echo "The Sum Of Two Dice Number $num_One + $num_Two : "$rel;
